4705063d7229dbe24157eb4e2c83cce1a570f456
Dustin Quam <dustinquam@me.com>
2016-01-30 00:29:22 -0600

    updatin

--- wp-content/themes/sage/templates/head.php
+++ wp-content/themes/sage/templates/head.php
@@ -2,5 +2,6 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <?php wp_head(); ?>
+<?php wp_head();?>
+<script type="text/javascript" src="//use.typekit.net/mxa8igh.js"></script> <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 </head>
