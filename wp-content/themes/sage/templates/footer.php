9ed9f17f4aeb641ed02bd6ebb112a063816b1b2b
jbrown40 <jessicabrown@dhcp80ff31c4.dynamic.uiowa.edu>
2016-02-03 16:38:41 -0600

    include html and sass content

--- wp-content/themes/sage/templates/footer.php
+++ wp-content/themes/sage/templates/footer.php
@@ -1,7 +1,50 @@
-<footer class="content-info">
-  <div class="container">
+<footer class="footer clearfix" role="contentinfo">
+<div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
   <!--   <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a>             is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC BY 3.0</a></div>
   	<div>Icon made by <a href="http://catalinfertu.com" title="Catalin Fertu">Catalin Fertu</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed under <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC BY 3.0</a></div> -->
-  </div>
+  <div class="row">
+		<div class="col-md-6 col-lg-5">
+			<a href="http://studentlife.uiowa.edu" class="hide-print footer-logo"><img src="wp-content/themes/sage/assets/images/division-bar/dosl-uiowa.png" alt="Division Of Student Life"></a><br>
+				<p>The Division of Student Life fosters student success by creating and promoting inclusive educationally purposeful services and activities within and beyond the classroom.</p>
+			
+			<p>Address
+					<br />Phone: 888-888-8888
+					<br />Email: <a href="mailto:{$SiteConfig.EmailAddress}">emailaddress@something.com</a>
+			</p>
+		</div>
+		<div class="col-md-6 col-lg-4">
+			<div class="row">
+				<div class="col-sm-6">
+					<ul class="border-list">
+							<li><a href="http://localhost:888/krui-wp/listen/">Listen</a></li>
+							<li><a href="http://localhost:888/krui-wp/music/">Music</a></li>
+							<li><a href="http://localhost:888/krui-wp/news/">News</a></li>
+							<li><a href="http://localhost:888/krui-wp/sports/">Sports</a></li>
+							<li><a href="http://localhost:888/krui-wp/schedule/">Schedule</a></li>
+							<li><a href="http://localhost:888/krui-wp/about/">About</a></li>
+					</ul>
+				</div>
+				<div class="col-sm-6">
+ 					<ul class="border-list">
+							<li><a href="facebook.com" target="_blank"><span class="social-icon">F</span> Facebook</a></li>
+							<li><a href="twitter.com" target="_blank"><span class="social-icon">t</span> Twitter</a></li>
+							<li><a href="vimeo.com" target="_blank"><span class="social-icon">v</span> Vimeo</a></li>
+							<li><a href="youtube.com" target="_blank"><span class="social-icon">y</span> Youtube</a></li>
+							<li><a href="instagram.com" target="_blank"><span class="social-icon">i</span> Instagram</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-12 col-lg-3">
+				<p>Individuals with disabilities are encouraged to attend all University of Iowa - sponsored events. If you are a person with a disability who requires an accommodation in order to participate in this program, please contact the Center for Student Involvement and Leadership in advance at 319-335-3059.</p> 
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-12">
+			<hr>
+			<p>&copy; 2016 <a href="http://www.uiowa.edu/" target="_blank">The University of Iowa</a>. All Rights Reserved. <a href="http://www.uiowa.edu/homepage/online-privacy-information" target="_blank">Privacy Information</a></p>
+		</div>
+	</div>
+	</div>
 </footer>
